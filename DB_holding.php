a8f952fe5f41944ce3d9eabb33efa1eea3ec1c12
DaisyBell <daisybell@MacBook-Pro-de-DaisyBell.local>
2019-08-05 14:37:47 +0200

    init

---
+++ DB_holding.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * DB Holding
+ *
+ * Plugin Name: DB Holding Page
+ * Plugin URI:  https://daisybell.ch/wordpress/plugins
+ * Description: Give the ability to add a custom holding page
+ * Version:     1.0.0
+ * Author:      Sacha Beraud – DaisyBell
+ * Author URI:  https://daisybell.ch
+ * License:    	Copyright 2019, Sacha Beraud – DaisyBell. All rights reserved.  
+ * Text Domain: db-holding
+ */
